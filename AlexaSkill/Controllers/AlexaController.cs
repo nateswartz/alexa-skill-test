0ecb979827c02806918fffd7bef9e722faca3cec
Nate Swartzentruber <nswartzentruber@listrak.com>
2017-12-12 13:12:36 -0500

    Initial commit

---
+++ AlexaSkill/Controllers/AlexaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlexaSkill.Controllers
+{
+    public class AlexaController : Controller
+    {
+        [HttpPost]
+        [Route("api/alexa/test")]
+        public dynamic HelloTest(dynamic request)
+        {
+            return new
+            {
+                version = "1.0",
+                sessionAttributes = new { },
+                response = new
+                {
+                    outputSpeech = new
+                    {
+                        type = "PlainText",
+                        text = "Hello Swartzentrubers"
+                    },
+                    card = new
+                    {
+                        type = "Simple",
+                        title = "Hello World Test",
+                        content = "Hello\nSwartzentrubers!"
+                    },
+                    shouldEndSession = true
+                }
+            };
+        }
+    }
+}
