d3b03b0a1b050ff9ffc0396390ad68836856ef31
Nate Swartzentruber <nswartzentruber@listrak.com>
2017-12-13 12:41:39 -0500

    Add additional greetings and a yes/no response

--- AlexaSkill/Controllers/AlexaController.cs
+++ AlexaSkill/Controllers/AlexaController.cs
@@ -11,14 +11,30 @@ namespace AlexaSkill.Controllers
         [Route("api/alexa/test")]
         public AlexaResponse HelloTest(AlexaRequest request)
         {
-            var text = $"There are {GetDaysTillChristmas()} days left until Christmas. {GetChristmasGreeting()}.";
-            var content = $"There are {GetDaysTillChristmas()} days left until Christmas.  {GetChristmasGreeting()}.";
+            if (request.Request.Type == "LaunchRequest")
+            {
+                return new AlexaResponse("Welcome to Christmas Countdown, just say how many days until Christmas", false);
+            }
+            if (request.Request.Intent.Name == "CountdownIntent")
+            {
+                var text = $"There are {GetDaysTillChristmas()} days left until Christmas. {GetChristmasGreeting()}.  Are you excited?";
+                var content = $"There are {GetDaysTillChristmas()} days left until Christmas.  {GetChristmasGreeting()}.";
 
-            var response = new AlexaResponse(text, content);
-            response.Response.Card.Title = "Christmas Countdown 🎄";
-            response.Response.ShouldEndSession = true;
+                var response = new AlexaResponse(text, content);
+                response.Response.Card.Title = "🎄 Christmas Countdown 🎄";
+                response.Response.ShouldEndSession = false;
 
-            return response;
+                return response;
+            }
+            else if (request.Request.Intent.Name == "AMAZON.NoIntent")
+            {
+                return new AlexaResponse("Fine, you don't have to be such a grinch.", true);
+            }
+            else if (request.Request.Intent.Name == "AMAZON.YesIntent")
+            {
+                return new AlexaResponse("Good, I'm glad you're in the Christmas spirit.", true);
+            }
+            return new AlexaResponse("I don't know how to help with that", true);
         }
 
         private int GetDaysTillChristmas()
@@ -41,8 +57,10 @@ namespace AlexaSkill.Controllers
             {
                 "I can't wait",
                 "Deck the Halls",
-                "Ho, ho, ho",
-                "Fa la la la la, la la, la, la"
+                "Ho ho ho",
+                "Fa la la la la, la la, la, la",
+                "Just listen to those sleigh bells ring",
+                "I hope you've been good this year"
             };
 
             var randomNumber = new Random(DateTime.Now.Millisecond).Next(greetings.Count);
