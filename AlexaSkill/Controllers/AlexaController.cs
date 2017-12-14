08f06a5ebdb94c3a871fbae3e9eac85959623bfa
Nate Swartzentruber <nswartzentruber@listrak.com>
2017-12-14 12:57:24 -0500

    Various improvements
    Refactor to break up some of the logic
    Add additional sayings
    Make "are you excited?" prompt occur randomly
    Ignore yes/no if not a continuation of a session

--- AlexaSkill/Controllers/AlexaController.cs
+++ AlexaSkill/Controllers/AlexaController.cs
@@ -11,29 +11,60 @@ namespace AlexaSkill.Controllers
         [Route("api/alexa/test")]
         public AlexaResponse HelloTest([FromBody]AlexaRequest request)
         {
+            return HandleRequest(request);
+        }
+
+        private AlexaResponse HandleRequest(AlexaRequest request)
+        {
             if (request.Request.Type == "LaunchRequest")
             {
-                return new AlexaResponse("Welcome to Christmas Countdown, just say how many days until Christmas", false);
+                return HandleLaunchRequest(request);
             }
+            if (request.Request.Type == "IntentRequest")
+            {
+                return HandleIntentRequest(request);
+            }
+            return DefaultResponse();
+        }
+
+        private AlexaResponse HandleIntentRequest(AlexaRequest request)
+        {
             if (request.Request.Intent.Name == "CountdownIntent")
             {
-                var text = $"There are {GetDaysTillChristmas()} days left until Christmas. {GetChristmasGreeting()}.  Are you excited?";
+                var shouldEnd = new Random(DateTime.Now.Millisecond).Next(3) != 1 ? true : false;
+
+                var text = $"There are {GetDaysTillChristmas()} days left until Christmas. {GetChristmasGreeting()}.";
                 var content = $"There are {GetDaysTillChristmas()} days left until Christmas.  {GetChristmasGreeting()}.";
+
+                if (!shouldEnd)
+                {
+                    text += "Are you excited?";
+                }
 
                 var response = new AlexaResponse(text, content);
                 response.Response.Card.Title = "🎄 Christmas Countdown 🎄";
-                response.Response.ShouldEndSession = false;
+                response.Response.ShouldEndSession = shouldEnd;
 
                 return response;
             }
-            else if (request.Request.Intent.Name == "AMAZON.NoIntent")
+            else if (request.Request.Intent.Name == "AMAZON.NoIntent" && !request.Session.New)
             {
                 return new AlexaResponse("Fine, you don't have to be such a grinch.", true);
             }
-            else if (request.Request.Intent.Name == "AMAZON.YesIntent")
+            else if (request.Request.Intent.Name == "AMAZON.YesIntent" && !request.Session.New)
             {
                 return new AlexaResponse("Good, I'm glad you're in the Christmas spirit.", true);
             }
+            return DefaultResponse();
+        }
+
+        private AlexaResponse HandleLaunchRequest(AlexaRequest request)
+        {
+            return new AlexaResponse("Welcome to Christmas Countdown, just say how many days until Christmas", false);
+        }
+
+        private AlexaResponse DefaultResponse()
+        {
             return new AlexaResponse("I don't know how to help with that", true);
         }
 
@@ -60,7 +91,9 @@ namespace AlexaSkill.Controllers
                 "Ho ho ho",
                 "Fa la la la la, la la, la, la",
                 "Just listen to those sleigh bells ring",
-                "I hope you've been good this year"
+                "I hope you've been good this year",
+                "Ugly Christmas sweaters for everyone",
+                "It's the most wonderful time of the year"
             };
 
             var randomNumber = new Random(DateTime.Now.Millisecond).Next(greetings.Count);
